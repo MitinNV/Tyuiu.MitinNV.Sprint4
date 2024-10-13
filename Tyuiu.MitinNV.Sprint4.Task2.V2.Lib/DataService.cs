5be77aa8de37afc2d513964f9396845cb267af44
MitinNV <tyuiu.mitinnv@outlook.com>
2024-10-13 23:34:00 +0500

    .

---
+++ Tyuiu.MitinNV.Sprint4.Task2.V2.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MitinNV.Sprint4.Task2.V2.Lib
+{
+    public class DataService : ISprint4Task2V2
+    {
+        public int Calculate(int[] array)
+        {
+            int res = 1;
+            foreach (var item in array)
+            {
+                if (item % 2 == 1)
+                {
+                    res *= item;
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MitinNV.Sprint4.Task2.V2/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.MitinNV.Sprint4.Task2.V2.Lib;
+namespace Tyuiu.MitinNV.Sprint4.Task2.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Митин Никита Владимирович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Одномерные массивы. (генератор случайных чисел)                         *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #2                                                            *");
+            Console.WriteLine("* Выполнил: Митин Никита Владимирович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дан одномерный целочисленный массив на 11 элементов заполненный случайным*");
+            Console.WriteLine("*ми в диапазоне от 1 до 8 подсчитать произведение нечетных элементов масси*");
+            Console.WriteLine("*ива.                                                                     *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            string a;
+            a = Console.ReadLine();
+            var b = new int[a.Split(", ").Count()];
+            var i = 0;
+            foreach (var item in a.Split(", "))
+            {
+                b[i] = Convert.ToInt32(item);
+                i++;
+            }
+            var result = ds.Calculate(b);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
